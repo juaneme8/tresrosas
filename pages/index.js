8fbef7831a4759602cf4255915195a174cefcf33
Juan Ocho <juaneme8@gmail.com>
2021-04-25 13:15:44 -0300

    Installed Next.js with npx

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+
+  return (
+    <>
+      <Head>
+        <title>Tres Rosas</title>
+      </Head>
+      <h1>Próximamente</h1>
+    </>
+  )
+}
